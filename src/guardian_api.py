686e09a8344a781798e3fb2038c367c34a6c3c83
Saif <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-01-17 19:17:50 +0000

    added api call

---
+++ src/guardian_api.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+def build_api_url(arg_1:str = "tech") -> str:
+    return f"https://content.guardianapis.com/search?q={arg_1}&api-key=test"
+
+def fetch_api(url:str) -> json:
+    return requests.get(url).json()
+
+def output_to_json_file(jsn:json, file_path='./default.json') -> None:
+    with open(file_path, 'w') as f:
+        json.dump(jsn, f, indent=4)
+
+def get_guardian_data(arg_1:str, arg_2:str = None) -> None:
+    if arg_2:
+        file_path = f"./{arg_2}.json"
+        output_to_json_file(fetch_api(build_api_url(arg_1, arg_2)), file_path)
+    else:
+        output_to_json_file(fetch_api(build_api_url(arg_1)))
+
+get_guardian_data("tech")
