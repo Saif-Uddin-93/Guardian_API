5182b33d1e91967b5b5f2da2a674c0ac80c68435
Saif <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-02-12 16:17:15 +0000

    tetsted receiving json strings from sqs

--- test/separate_tests/test_receive.py
+++ test/separate_tests/test_receive.py
@@ -1,4 +1,7 @@
-from utility.aws_utils import create_sqs_queue, send_message_to_sqs, receive_messages_from_sqs
+import json
+from utility.aws_utils import create_sqs_queue, send_message_to_sqs, \
+    receive_messages_from_sqs
+from api_handler.api_utils import fetch_api, build_api_url, output_to_json_file
 
 
 # def test_connect_to_rabbitmq_server():
@@ -10,5 +13,13 @@ def test_recieve_messages_from_sqs(sqs_client):
     message = "Test message."
     send_message_to_sqs(url = queue_url, message = message, client = sqs_client)
     queue_messages = receive_messages_from_sqs(queue_url, sqs_client)
-    print(queue_messages)
+    # print(queue_messages)
     assert queue_messages[0]["Body"] == message
+
+def test_receive_json_from_sqs(sqs_client):
+    json_response : json = json.dumps(fetch_api(build_api_url("tech")))
+    queue = create_sqs_queue(queue_name = "Test", client = sqs_client)
+    queue_url = queue["QueueUrl"]
+    send_message_to_sqs(url = queue_url, message = json_response, client = sqs_client)
+    queue_messages = receive_messages_from_sqs(queue_url, sqs_client)
+    assert queue_messages[0]["Body"] == json_response
