56a22e2163afa4d476917a7d2cb87e6da656ed1a
Saif <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-03-06 10:42:55 +0000

    added rating checkbox functionality

--- src/script.js
+++ src/script.js
@@ -1,5 +1,20 @@
 var searchButton = $("#search-button");
+var ratingCheckbox = $("#rating-check");
+
 
 searchButton.on("click", function(){
     console.log($("#search-term").val());
+    console.log($("#date-from").val());
+    console.log($("#date-to").val());
+    console.log($("#page-size").val());
+    console.log($("#rating").val());
 });
+
+
+ratingCheckbox.on("click", function(){
+    if(ratingCheckbox.prop("checked")){
+        $("#rating").prop("disabled", false);
+    }else{
+        $("#rating").prop("disabled", true);
+    }
+})
