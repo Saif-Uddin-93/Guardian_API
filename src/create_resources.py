b38e915f1d5ebe8d8bb74f37a93a610c5aeed751
Saif <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-03-20 04:39:01 +0000

    moved files

--- src/create_resources.py
+++ src/create_resources.py
@@ -4,4 +4,4 @@ from utility.aws_utils import *
 iam_role_id, iam_role_arn = create_iam_role()
 create_sqs_queue("guardian-queue")
 create_api("guardian-api")
-create_lambda()
+create_lambda(iam_role_id, iam_role_arn)
