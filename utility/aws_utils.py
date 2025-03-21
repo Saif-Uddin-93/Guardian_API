5156e7f9864ddca45c8156d6021e94c11474cd23
Saif <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-03-21 01:09:02 +0000

    added logging

--- utility/aws_utils.py
+++ utility/aws_utils.py
@@ -1,4 +1,4 @@
-import boto3, json
+import boto3, json, logging
 from botocore.exceptions import ClientError, BotoCoreError
 
 region_name = "eu-west-2"
@@ -25,6 +25,11 @@ ASSUMED_ROLE_SESSION = boto3.Session(
     aws_session_token=assumed_role_credentials['SessionToken'],
     region_name=region_name
 )
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+logger.info(f"LOGGING access key: {assumed_role_credentials['AccessKeyId']}")
 
 iam_client = ASSUMED_ROLE_SESSION.client('iam', region_name=region_name)   
 
