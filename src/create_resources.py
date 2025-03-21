42b2282e10223244a59e0433ded0a6266ff53d18
Saif <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-03-21 00:35:05 +0000

    removed create_iam

--- src/create_resources.py
+++ src/create_resources.py
@@ -1,7 +1,5 @@
 from utility.aws_utils import *
 
-
-iam_role_id, iam_role_arn = create_iam_role()
 create_sqs_queue("guardian-queue")
 create_api("guardian-api")
-create_lambda(iam_role_id, iam_role_arn)
+create_lambda()
