ee134607a0c3cc548e407d8f28c735b050416fae
Saif <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-03-22 14:30:26 +0000

    removed modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,11 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-#   backend "s3" {
-#     bucket = "launchpad-de-guardian-api-tfstate"
-#     key    = "terraform/terraform.tfstate"
-#     region = "eu-west-2"
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
 provider "aws" {
   region = "eu-west-2"
