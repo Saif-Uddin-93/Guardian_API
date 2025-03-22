f6ec912084fe21ae8ee74e9940fd35051510cbdd
Saif <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-03-22 14:31:17 +0000

    updated lambda resources

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -15,18 +15,35 @@ data "archive_file" "sqs_send_lambda" {
   output_path      = "${path.module}/../lambda_handlers/sqs_send.zip"
 }
 
+data "archive_file" "sqs_receive_lambda" {
+  type             = "zip"
+  source_file      = "${path.module}/../lambda_handlers/sqs_receive.py"
+  output_file_mode = "0666"
+  output_path      = "${path.module}/../lambda_handlers/sqs_receive.zip"
+}
+
 resource "aws_lambda_function" "sqs_send_lambda" {
   filename         = "${path.module}/../lambda_handlers/sqs_send.zip"
   function_name    = "sqs_send"
   role             = aws_iam_role.guardian_iam_role.arn
   handler          = "sqs_send.lambda_handler"
-  source_code_hash = data.archive_file.sqs_send_lambda.output_base64sha256
+#   source_code_hash = data.archive_file.sqs_send_lambda.output_base64sha256
   runtime          = "python3.13"
-  timeout          = 60
+  timeout          = 10
   layers           = [
     aws_lambda_layer_version.requests_layer.arn,
     aws_lambda_layer_version.utility_layer.arn,
   ]
+  tracing_config {
+    mode = "PassThrough"
+  }
+  logging_config {
+    log_format = "Text"
+    log_group  = "/aws/lambda/sqs_send"
+  }
+  ephemeral_storage {
+    size = 512
+  }
 }
 
 resource "aws_lambda_function" "sqs_receive_lambda" {
@@ -34,12 +51,22 @@ resource "aws_lambda_function" "sqs_receive_lambda" {
   function_name    = "sqs_receive"
   role             = aws_iam_role.guardian_iam_role.arn
   handler          = "sqs_receive.lambda_handler"
-  source_code_hash = data.archive_file.sqs_receive_lambda.output_base64sha256
+#   source_code_hash = data.archive_file.sqs_receive_lambda.output_base64sha256
   runtime          = "python3.13"
-  timeout          = 60
+  timeout          = 10
   layers           = [
     aws_lambda_layer_version.utility_layer.arn,
   ]
+  tracing_config {
+    mode = "PassThrough"
+  }
+  logging_config {
+    log_format = "Text"
+    log_group  = "/aws/lambda/sqs_receive"
+  }
+  ephemeral_storage {
+    size = 512
+  }
 }
 
 # {
