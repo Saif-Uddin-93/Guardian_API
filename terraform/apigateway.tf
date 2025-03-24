e7b94292f5d8a3828f64bc01d3b54791c0cefded
Saif <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-03-24 14:25:48 +0000

    updated terraform

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -37,8 +37,9 @@ resource "aws_api_gateway_integration" "guardian_integration" {
   rest_api_id             = aws_api_gateway_rest_api.guardian_api.id
   resource_id             = aws_api_gateway_resource.guardian_resource.id
   http_method             = aws_api_gateway_method.guardian_method.http_method
-  integration_http_method = "PUT"
-  type                    = "AWS_PROXY"
+  integration_http_method = "GET"
+  type                    = "AWS"
+  passthrough_behavior    = "WHEN_NO_TEMPLATES"
   uri                     = aws_lambda_function.sqs_send_lambda.invoke_arn
 
   request_templates = {
