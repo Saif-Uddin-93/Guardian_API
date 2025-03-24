e631436f925bbfa9ed37a6046880fb9c840c03a8
Saif <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-03-24 12:33:03 +0000

    update terraform

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_log_group" "api_logs" {
+resource "aws_cloudwatch_log_group" "apigw_log_group" {
   name              = "/aws/apigateway/guardian-api"
   retention_in_days = 30
 }
@@ -11,6 +11,11 @@ resource "aws_cloudwatch_log_group" "sqs_send_log_group" {
 resource "aws_cloudwatch_log_group" "sqs_receive_log_group" {
   name              = "/aws/lambda/guardian_sqs_receive"
   retention_in_days = 30
+}
+
+resource "aws_cloudwatch_log_stream" "apigw_log_stream" {
+  name           = "apigw_log_stream"
+  log_group_name = aws_cloudwatch_log_group.apigw_log_group.name
 }
 
 resource "aws_cloudwatch_log_stream" "sqs_send_log_stream" {
