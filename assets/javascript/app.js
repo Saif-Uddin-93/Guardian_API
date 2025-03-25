23e4d1c8d9b77277612a0734a13f14a87f0e3179
Saif Uddin <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-03-25 02:08:05 +0000

    updated frontend

---
+++ assets/javascript/app.js
@@ -0,0 +1,56 @@
+/* eslint-disable prettier/prettier */
+$(document).ready(function () {
+
+    // var apiURL = "https://gnews.io/api/v4/search?";
+    // var key = "&country=us&max=10&token=d06b56befd778f95afde57c26ebc9890";
+    var key = ""
+    var searchString = "";
+    // var queryURL;
+    var articleNumber = 0;
+
+    const build_api_url = (term="tech", opts=[]) => {
+        searchTerm = $("#search-string").val() ? $("#search-string").val() : term
+        filters = ""
+        opts.forEach(keyVal => {
+            filters += `${keyVal[0]}=${keyVal[1]}&`
+        });
+        return `https://content.guardianapis.com/search?q=${searchTerm}&show-blocks=body&${filters}api-key=${key || 'test'}`
+    }
+
+    $(".clear").click(function () {
+        articleNumber = 0;
+        $("#search-string").val("");
+        $("#article-results").empty();
+    });
+
+    $(".search").on("click", function () {
+        $("#article-results").empty();
+        articleNumber = 0;
+        searchString = $("#search-string").val();
+        // queryURL = apiURL + "q=" + searchString + key;
+        // console.log('query: ', queryURL)
+
+        fetch(build_api_url(opts = [["from-date", "2020-01-01"], ["page-size", "1"]]))
+            .then(function (response) {
+                return response.json();
+            }).then(function (data) {
+                console.log(data);
+                console.log(data.response.results);
+                for (i = 0; i < data.response.results.length; i++) {
+                    articleNumber++;
+                    var article = $("<div>");
+                    article.addClass("well well-lg row");
+                    var title = $("<h3>");
+                    title.addClass("title");
+                    title.text(data.response.results[i].webTitle);
+                    var description = $("<div>");
+                    description.addClass("description");
+                    description.html(data.response.results[i].blocks.body[0].bodyHtml);
+                    console.log(description)
+                    var number = $("<div class='articleNumber'>").text(articleNumber);
+                    $(article).append(number, title, description);
+                    $("#article-results").append(article);
+                }
+            });
+    });
+});
