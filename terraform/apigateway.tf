91bedb37be75ff6f6ef468089f80242fd69cf7d8
Saif <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-03-25 21:52:52 +0000

    updated apigateway

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -11,15 +11,15 @@ resource "aws_api_gateway_rest_api" "guardian_api" {
   }
 }
 
-resource "aws_api_gateway_resource" "guardian_resource" {
-  rest_api_id = aws_api_gateway_rest_api.guardian_api.id
-  parent_id   = aws_api_gateway_rest_api.guardian_api.root_resource_id
-  path_part   = "data"
-}
+# resource "aws_api_gateway_resource" "guardian_resource" {
+#   rest_api_id = aws_api_gateway_rest_api.guardian_api.id
+#   parent_id   = aws_api_gateway_rest_api.guardian_api.root_resource_id
+#   path_part   = "/"
+# }
 
 resource "aws_api_gateway_method" "guardian_method" {
   rest_api_id   = aws_api_gateway_rest_api.guardian_api.id
-  resource_id   = aws_api_gateway_resource.guardian_resource.id
+  resource_id   = aws_api_gateway_rest_api.guardian_api.root_resource_id
   http_method   = "GET"
   authorization = "NONE"
 
@@ -35,7 +35,7 @@ resource "aws_api_gateway_method" "guardian_method" {
 
 resource "aws_api_gateway_method_response" "get_method_response" {
   rest_api_id = aws_api_gateway_rest_api.guardian_api.id
-  resource_id = aws_api_gateway_resource.guardian_resource.id
+  resource_id = aws_api_gateway_rest_api.guardian_api.root_resource_id
   http_method = aws_api_gateway_method.guardian_method.http_method
   status_code = "200"
 
@@ -48,7 +48,7 @@ resource "aws_api_gateway_method_response" "get_method_response" {
 
 resource "aws_api_gateway_integration" "guardian_integration" {
   rest_api_id             = aws_api_gateway_rest_api.guardian_api.id
-  resource_id             = aws_api_gateway_resource.guardian_resource.id
+  resource_id             = aws_api_gateway_rest_api.guardian_api.root_resource_id
   http_method             = aws_api_gateway_method.guardian_method.http_method
   integration_http_method = "GET"
   type                    = "AWS"
@@ -66,7 +66,7 @@ EOF
 
 resource "aws_api_gateway_integration_response" "get_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.guardian_api.id
-  resource_id = aws_api_gateway_resource.guardian_resource.id
+  resource_id = aws_api_gateway_rest_api.guardian_api.root_resource_id
   http_method = aws_api_gateway_method.guardian_method.http_method
   status_code = aws_api_gateway_method_response.get_method_response.status_code
 
