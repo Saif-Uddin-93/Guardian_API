2c9e0380103232c69cc596f0d199ef124ebebabd
Saif <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-03-26 11:44:59 +0000

    locals moved in to main

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,8 @@ provider "aws" {
 data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
+
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+  region = data.aws_region.current.name
+}
