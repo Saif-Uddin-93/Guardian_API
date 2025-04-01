2ddad1b0d5dd8e03a04bea0e4913d209bbae962c
Saif <106098927+Saif-Uddin-93@users.noreply.github.com>
2025-04-01 14:52:20 +0100

    added compatible architectures

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,15 +1,17 @@
 resource "aws_lambda_layer_version" "requests_layer" {
-  layer_name          = "requests"
-  filename            = "${path.module}/../requests_layer.zip"
-  compatible_runtimes = ["python3.13"]
-  description         = "Requests layer for Lambda functions"
+  layer_name               = "requests"
+  filename                 = "${path.module}/../requests_layer.zip"
+  compatible_runtimes      = ["python3.13"]
+  compatible_architectures = ["arm64", "x86_64"]
+  description              = "Requests layer for Lambda functions"
 }
 
 resource "aws_lambda_layer_version" "utility_layer" {
-  layer_name          = "utility"
-  filename            = "${path.module}/../utility_layer.zip"
-  compatible_runtimes = ["python3.13"]
-  description         = "Utility layer for Lambda functions"
+  layer_name               = "utility"
+  filename                 = "${path.module}/../utility_layer.zip"
+  compatible_runtimes      = ["python3.13"]
+  compatible_architectures = ["arm64", "x86_64"]
+  description              = "Utility layer for Lambda functions"
 }
 
 data "archive_file" "sqs_send_lambda" {
